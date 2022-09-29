d346bd2eb56ff9e224994658587d8f848452165a
Chenhao Liu <K21077228@kcl.ac.uk>
2022-09-29 20:26:08 +0100

    make the function work

--- primes.py
+++ primes.py
@@ -3,20 +3,22 @@
 import math as m
 
 def primes(number_of_primes):
-    list = []
+    list = [2, 3]
     counter = 4
 
     if number_of_primes < 1:
         return []
     elif number_of_primes == 1:
         return [2]
+    elif number_of_primes == 2:
+        return [2, 3]
     while len(list) < number_of_primes:
         isPrime = True
-        for i in range(2, int(m.sqrt(counter))):
+        for i in range(2, int(m.sqrt(counter) + 1)):
             if counter % i == 0:
                 isPrime = False
                 break
         if isPrime:
             list.append(counter)
-            counter += 1
+        counter += 1
     return list
